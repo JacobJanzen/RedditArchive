30f5388a5fefad0f5b58c6f577d29ec79451804a
Jacob Janzen <jjanzen@cscscience.ca>
2023-06-14 12:18:19 -0500

    bug fixes

--- archive.js
+++ archive.js
@@ -1,12 +1,13 @@
 const reddit = new RegExp("^https?://(www\.|old\.)?reddit\.com");
 if (reddit.test(window.location.href)) {
-    console.log("TEST")
     window.location.replace("https://web.archive.org/web/*/" + window.location.href);
 } else {
     newURL = window.location.href.split("*", 2)[1].substr(1);
-    fetch("https://web.archive.org/wayback/available?url="+newURL)
-        .then(response => response.json())
-        .then(response => window.location.replace(response.archived_snapshots.closest.url))
+    if (reddit.test(newURL)) {
+        fetch("https://web.archive.org/wayback/available?url="+newURL)
+            .then(response => response.json())
+            .then(response => window.location.replace(response.archived_snapshots.closest.url))
+    }
 }
 
 
